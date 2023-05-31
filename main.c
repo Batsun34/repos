11118ba91ed9feb1ef1cba2fff68e112cadf86d4
Batsun <kkazania34@gmail.com>
2023-06-01 00:48:22 +0300

    created

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "sdt.h"
+#include <stdio.h>
+
+int main ()
+{
+    int first = 0;
+    int second = 0;
+
+    printf("Input 1 num:\n");
+    scanf("%d", &first);
+
+    printf("Input 2 num:\n");
+    scanf("%d", &second);
+}
